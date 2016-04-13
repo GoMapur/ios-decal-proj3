21d200eefada343ac054a1b0afb5598946508299
Mapur <Mapur.lu@gmail.edu>
2016-04-13 02:07:44 -0700

    .DS_Store banished!

---
+++ Photos/Photos/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photos
+//
+//  Created by Mingjian Lu on 4/12/16.
+//  Copyright © 2016 iOS DeCal. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
